2f88976d8a29b7cf1fd747c7a29decd0a95dca31
MOMOONS <quinten1.moons@gmail.com>
2023-11-22 16:44:42 +0100

    added music and rock

--- Revalidation/Assets/Scripts/TouchObject.cs
+++ Revalidation/Assets/Scripts/TouchObject.cs
@@ -8,6 +8,8 @@ public class TouchObject : MonoBehaviour
     public float Rotation = 0f;
     public Vector3 RockDirection = new Vector3(0f, 0f, 1f);
     public Vector3 ShipDirection = new Vector3(0f, 0f, 1f);
+    public Vector3 BalloonDirection = new Vector3(0f, 0f, -1f);
+    public Vector3 SeagullDirection = new Vector3(0f, 0f, -1f);
 
     void Start()
     {
@@ -16,6 +18,18 @@ public class TouchObject : MonoBehaviour
         if (transform.tag == "Ship")
         {
             transform.Rotate(new Vector3(90f, 0, 0));
+        }
+
+        if (transform.tag == "Balloon")
+        {
+            transform.Translate(new Vector3(1.5f, 2.5f, 0));
+            transform.Rotate(new Vector3(-90f, 0, 0));
+        }
+
+        if (transform.tag == "Seagull")
+        {
+            transform.Translate(new Vector3(3.157137f, 0, 7.726573f));
+            transform.Rotate(new Vector3(0, 180f, 0));
         }
     }
 
@@ -33,6 +47,14 @@ public class TouchObject : MonoBehaviour
         else if (transform.tag == "Ship")
         {
             transform.Translate(ShipDirection * Speed * Time.deltaTime, Space.World);
+        }
+        else if (transform.tag == "Balloon")
+        {
+            transform.Translate(BalloonDirection * Speed * Time.deltaTime, Space.World);
+        }
+        else if (transform.tag == "Seagull")
+        {
+            transform.Translate(SeagullDirection * Speed * Time.deltaTime, Space.World);
         }
         else
         {
