16f7e6bc95e7b33279e017eff791893ff973d3a6
VictorDeDecker <124567912+VictorDeDecker@users.noreply.github.com>
2023-11-26 21:23:39 +0100

    Victor (#8)
    * added music assets
    
    * implement new script in level 2
    
    * remove angular files
    
    * added seagull and hot air balloon
    
    * add main menu + submarine buttons
    
    * added spawnplane
    
    * added music and rock
    
    * added delete scene
    
    * add scene changing capability v1, update scripts and website
    
    * add scene change to site, add mainthreaddispatcher, update unityserver class
    
    * added despawn plane and added riggid body and box collider to the balloon and the seagull
    
    * fix error after changing scenes and changing parameters
    
    * fix merge conflict
    
    * fix merge conflicts and update level1
    
    * update documentation
    
    ---------
    
    Co-authored-by: MOMOONS <quinten1.moons@gmail.com>

---
+++ Revalidation/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+    //public string sceneToLoad;
+
+    public void LoadLevel(string sceneToLoad)
+    {
+        SceneManager.LoadScene(sceneToLoad);
+    }
+
+    public void QuitLevel()
+    {
+        Application.Quit();
+    }
+}
