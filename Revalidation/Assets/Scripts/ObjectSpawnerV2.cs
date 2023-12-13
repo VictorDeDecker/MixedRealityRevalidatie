c981dfd55e0fe2b197acbc40bc0675546e5f67cd
Len <lenmertens11@gmail.com>
2023-12-13 16:04:35 +0100

    New object spawner, again (#15)
    * feat: duckmode and 2 layers
    
    * feat: ObjectSpawnerV2 bare bones
    
    * fix: parameters in scene to better fit

---
+++ Revalidation/Assets/Scripts/ObjectSpawnerV2.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectSpawnerV2 : MonoBehaviour
+{
+    public List<TouchObject> Objects;
+    public int LevelLengthInSec = 180;
+    public int TimeBetweenSpawnsInSec = 2;
+    public bool InfiniteSpawn = false;
+    public float Height;
+    public float SpawnRadius;
+    public float SpaceBetween;
+    public bool IncludeDucking = false;
+    public bool IncludeMovement = false;
+
+
+    void Start()
+    {
+        StartCoroutine(StartSpawning());
+    }
+
+    private IEnumerator StartSpawning()
+    {
+        
+        while (LevelLengthInSec > 0 || InfiniteSpawn)
+        {
+            LevelLengthInSec--;
+            SpawnObject();
+            yield return new WaitForSeconds(TimeBetweenSpawnsInSec);
+        }
+    }
+
+    private void SpawnObject()
+    {
+        var spawnObject = Objects[Mathf.FloorToInt(Objects.Count * Random.value)];
+
+        Instantiate(spawnObject.gameObject, RandomSpawnPoint(), Quaternion.identity);
+    }
+
+    private Vector3 RandomSpawnPoint()
+    {
+        Vector3 point = Random.value > 0.5 ? 
+            (Vector3)Random.insideUnitCircle.normalized * Random.Range(0, SpawnRadius) + new Vector3(SpaceBetween / 2, 0, 0) : 
+            (Vector3)Random.insideUnitCircle.normalized * Random.Range(0, SpawnRadius) + new Vector3(- SpaceBetween / 2, 0, 0);
+
+        return point + this.gameObject.transform.position + new Vector3(0 , Height, 0);
+    }
+}
